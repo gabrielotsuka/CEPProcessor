d6574dfd5793bcf71614f20e5466303766ccb398
gabrielotsuka <gabrielotsuka@gmail.com>
2021-03-12 08:40:08 -0300

    start skeleton

--- ex01.c
+++ ex01.c
@@ -2,6 +2,27 @@
 
 int main() {
 
+	FILE* inputFile, outputFile;
+	char* line = NULL;
+	size_t len = 0;
+
+	inputFile = fopen("cep.txt", "r");
+	outputFile = fopen("/answers/ex01.txt", "w");
+
+	if (inputFile == NULL || outputFile == NULL) {
+		printf("Falha na leitura dos arquivos\n");
+		exit(1);
+	}
+
+	while (getline(&line, &len, inputFile) != -1) {
+		outputLine = formatString(line);
+	}
+
+
 	printf("Minha parte ta feita, agr é só terminar Marquin <3\n");
 	return 0;
+}
+
+char* formatString(char* inputLine) {
+
 }
