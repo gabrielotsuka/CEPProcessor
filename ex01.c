a3d23bffe13dd6060e6aff55873f00a917663d5c
Marcos Felipe <marcosfellipecosta@gmail.com>
2021-03-18 09:38:53 -0300

    wip

--- ex01.c
+++ ex01.c
@@ -2,39 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-void formatString(char* inputLine);
+char* formatString(char* inputLine);
 
 int main() {
 
 	FILE* inputFile;
 	FILE* outputFile;
-	char line[1234];
+	char line[150];
 	size_t len = 0;
 
 	inputFile = fopen("cep.txt", "r");
 	outputFile = fopen("output.txt", "w+");
 
-	if (inputFile == NULL || outputFile == NULL) {
+	while (inputFile == NULL || outputFile == NULL) {
 		printf("Falha na leitura dos arquivos\n");
 		exit(1);
 	}
 
-	if (fgets(line, 500, inputFile) != NULL) {
+	while (fgets(line, 150, inputFile) != NULL) {
 		line[strlen(line)-1] = '\0';
-		formatString(line);
-		
+
+		char newLine[150];
+		char cep[15], estado[3], cidade[39], rua[67];
+		sscanf(line, "%[^\t]\t%[^\t]\t%[^\t]\t%[^\r]", cep, estado, cidade, rua);
+		sprintf(newLine, "%s|%s|%s|%s\n", rua, cidade, estado, cep);
+
+		fwrite(newLine, sizeof(char), sizeof(newLine), outputFile);
 	}
+
+	fclose(inputFile);
+	fclose(outputFile);
 	return 0;
 }
 
-void formatString(char* inputLine) {
-	char newLine[12344];
-	char cep[1234], estado[1234], cidade[1234], rua[1234];
-	sscanf(inputLine, "%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]", cep, estado, cidade, rua);
-
-	printf("%s|%s|%s|%s\n", cep, estado, cidade, rua);
-	
-
-	printf("%s\n", newLine);
-	// return newLine;
+char* formatString(char* inputLine) {
+	char * newLine;
+	char cep[15], estado[3], cidade[39], rua[67];
+	sscanf(inputLine, "%[^\t]\t%[^\t]\t%[^\t]\t%[^\r]", cep, estado, cidade, rua);
+	sprintf(newLine, "%s|%s|%s|%s\n", rua, cidade, estado, cep);
+	return newLine;
 }
