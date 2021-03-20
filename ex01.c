9ec7d4c645a9bb03bc8b8a47a054a1893b69ebb7
gabrielotsuka <gabrielotsuka@gmail.com>
2021-03-20 11:07:59 -0300

    wip

--- ex01.c
+++ ex01.c
@@ -8,20 +8,16 @@ FILE* outputFile;
 
 char* formatLine(char* line);
 void transcribeCepFile(void);
-// void searchMenu(void);
-// void processOption(int option);
+void searchMenu(void);
+void processOption(int option);
+int hasSubstring(char* string, char* subString);
+void searchByCEP(void);
 
 int main() {
 
 	transcribeCepFile();
-	// searchMenu();
+	searchMenu();
 
-	char line[LINE_SIZE];
-	while (fgets(line, LINE_SIZE, outputFile) != NULL) {
-		
-		
-	}
-	
 	fclose(outputFile);
 	return 0;
 }
@@ -60,33 +56,64 @@ char* formatLine(char* line) {
 	return formattedLine; 
 }
 
-// void searchMenu(void) {
-// 	int option;
-// 	do {
-// 		printf("----Menu de busca----\n");
-// 		printf("|   1- CEP           |\n");
-// 		printf("|   2- Estado        |\n");
-// 		printf("|   3- Cidade        |\n");
-// 		printf("|   4- Logradouro    |\n");
-// 		printf("|   5- Sair          |\n");
-// 		printf("---------------------\n\n");
-// 		printf("Opcao desejada: ");
+void searchMenu(void) {
+	int option;
+	do {
+		printf("----Menu de busca----\n");
+		printf("|   1- CEP           |\n");
+		printf("|   2- Estado        |\n");
+		printf("|   3- Cidade        |\n");
+		printf("|   4- Logradouro    |\n");
+		printf("|   5- Sair          |\n");
+		printf("---------------------\n\n");
 
-// 		scanf("%d", &option);
-
-// 		char* foundPlace = malloc(LINE_SIZE);
-// 		foundPlace = processOption(option);
-// 	} while(option != 5);
+		printf("Insira o numero da opcao desejada: ");
+		scanf("%d", &option);
+		
+		processOption(option);
+	} while(option != 5);
 	
-// 	printf("Abrass!\n");
-// }
+	printf("Abrass!\n");
+}
 
-// char* processOption(int option) {
-// 	switch(option) {
-// 		case 1:
-// 			searchByField()
-// 			break
-// 	}
-// }
+void processOption(int option) {
+	switch(option) {
+		case 1:
+			searchByCEP();
+			break;
+	}
+}
+
+void searchByCEP(void) {
+	printf("Insira o valor a ser buscado: ");
+
+	char subString[LINE_SIZE];
+	fflush(stdin);
+	fgets(subString, LINE_SIZE, stdin);
+
+	char line[LINE_SIZE], lineCopy[LINE_SIZE];
+	while (fgets(line, LINE_SIZE, outputFile) != NULL) {
+		strcpy(lineCopy, line);
+		char* street = strtok(line, "|");
+		char* city = strtok(NULL, "|");
+		char* uf = strtok(NULL, "|");
+		char* cep = strtok(NULL, "\n\r");
+	
+		if (cep == NULL){
+			if (hasSubstring(uf, lineCopy) == 1) 
+				printf("%s\n", lineCopy);
+		} 
+		else {
+			if (hasSubstring(cep, lineCopy) == 1) 
+				printf("%s\n", lineCopy);
+		}
+	}
+} 	
 
 
+int hasSubstring(char* string, char* subString) {
+	if (strstr(string, subString) != NULL)
+		return 1;
+	else 
+		return 0;
+}
