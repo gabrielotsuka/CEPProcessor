7bd42a4423800cb7085b92cbd2ca098e3d0d1cb1
pedrohba1 <pedro.almeida@ufu.br>
2021-03-21 17:57:06 -0300

    exercicio 3 finalizado e o executavel

--- Exercicio3/ex03.c
+++ Exercicio3/ex03.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+void searchMenu(void);
+void accessPosition(int position);
+
 int main() {
+	searchMenu();
+	return 0;
+}
+
+
+void proessSearch(void) {
 	FILE* dataFile;
 	FILE* idxFile;
 
@@ -13,35 +22,51 @@ int main() {
 		printf("Falha na abertura dos arquivos\n");
 		exit(1);
 	}
-
-
-	// ler o índice de acordo com o número de bytes
 	int position;
+	printf("Insira a posição que deseja checar:");
+	scanf("%d", &position);
+	position--;
+	// ler o índice de acordo com o número de bytes
 	int idxStart;
 	int idxEnd;
 	// or exemplo, ler o item na terceira posição
-
-	position = 0;
 	fseek(idxFile, position*sizeof(int), SEEK_SET);
 	fread(&idxStart, sizeof(int), 1, idxFile);
-	printf("%d\n", idxStart);
 	
 	fseek(idxFile, (position+1)*sizeof(int), SEEK_SET);
 	fread(&idxEnd, sizeof(int), 1, idxFile);
-	printf("%d\n", idxEnd);
 
-	int MAXBUFFER = idxEnd - idxStart;
-	printf("%d\n", MAXBUFFER);
+	int MAXBUFFER = (idxEnd-1) - idxStart;
 
 	// pular para essa posição do cursor no arquivo resultado e ler a linha
 	fseek(dataFile, idxStart, SEEK_SET);
 
 	char buffer[MAXBUFFER];
 	int count = fread(&buffer, sizeof(char), MAXBUFFER, dataFile);
+	printf("=======================\n");
 	printf("%s\n", buffer);
-
+	printf("=======================\n");
+	
 	fclose(dataFile);
 	fclose(idxFile);
-	return 0;
 }
 
+
+
+void searchMenu(void) {
+	char charOption[4];
+	int option;
+	while(1) {
+		printf("|   1- Buscar por posição |\n");
+		printf("|   2- Sair               |\n");
+
+		printf("Insira o numero da opcao desejada: ");
+	 	scanf("%d", &option);
+	 	setbuf(stdin, NULL);
+		
+		if(option == 2) break;
+		if(option == 1) proessSearch();
+	}
+	
+	printf("Abrass!\n");
+}
