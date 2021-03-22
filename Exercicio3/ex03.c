6f4a04044476b19b41846ab294f493e7d359c947
pedrohba1 <pedro.almeida@ufu.br>
2021-03-21 21:52:14 -0300

    fix: junção dos arquivos

--- Exercicio3/ex03.c
+++ Exercicio3/ex03.c
@@ -4,11 +4,81 @@
 
 void searchMenu(void);
 void accessPosition(int position);
+char* formatLine(char* line);
+void transcribeCepFile(void);
+int  isAlphaNum(char c);
+void removeTrailingSpaces(char *line);
+#define LINE_SIZE 150
+
 
 int main() {
+	transcribeCepFile();
 	searchMenu();
 	return 0;
 }
+
+void transcribeCepFile(void) {
+	FILE* idxFile = fopen("data.idx","wb");
+	FILE* inputFile = fopen("cep.txt", "r");
+	FILE* outputFile = fopen("result.txt", "w+");
+
+	while (inputFile == NULL 
+		   || outputFile == NULL 
+		   ||  idxFile == NULL
+	) {
+		printf("Falha na abertura dos arquivos\n");
+		exit(1);
+	}
+
+	char line[LINE_SIZE];
+	//escrever a posição inicial do cursor
+	int cursor;
+	cursor = ftell(outputFile);
+	fwrite(& cursor, sizeof cursor, 1, idxFile);
+
+	while (fgets(line, LINE_SIZE, inputFile) != NULL) {
+ 		removeTrailingSpaces(line);
+        if(strlen(line) == 0) {
+        	continue;
+        }
+		fprintf(outputFile, "%s", formatLine(line));
+		cursor = ftell(outputFile);
+		fwrite(& cursor, sizeof cursor, 1, idxFile);
+	}
+
+	fclose(inputFile);
+	fclose(outputFile);
+	fclose(idxFile);
+}
+
+
+void removeTrailingSpaces(char *line) {
+    int sz = strlen(line);
+    for (int i=sz-1; i>=0; i--) {
+        if(isAlphaNum(line[i])) break;
+        line[i] = '\0'; 
+    }
+}
+
+int isAlphaNum(char c) {
+    if(c >= 'a' && c <= 'z') return 1;
+    if(c >= 'A' && c <= 'Z') return 1;
+    if(c >= '0' && c <= '9') return 1;
+    return 0;
+}
+
+char* formatLine(char* line) {
+	int cep;
+    char uf[3], cidade[39], logradouro[67] = "-";
+    sscanf(line, "%d\t%[^\t]\t%[^\t]\t%[^\t]", &cep, uf, cidade, logradouro);
+
+    char* formattedLine = malloc(LINE_SIZE);
+    sprintf(formattedLine, "%s|%s|%s|%d\n", logradouro, cidade, uf, cep);
+
+    return formattedLine;
+}
+
+
 
 
 void proessSearch(void) {
